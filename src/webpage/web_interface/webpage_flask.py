0f3e36a608eae7e1b362e5413b4b81fd6ba7f196
Emlyn Price <emlyn.je.price@gmail.com>
2018-12-25 22:17:41 +0000

    Make the plot actually work properly with templates

--- src/webpage/web_interface/webpage_flask.py
+++ src/webpage/web_interface/webpage_flask.py
@@ -3,7 +3,7 @@
 import sqlite3
 import datetime
 from bokeh.plotting import figure
-from bokeh.embed import json_item
+from bokeh.embed import json_item, components
 from bokeh.resources import CDN
 import json
 from flask import Flask, render_template
@@ -77,11 +77,39 @@ def plot_data():
     #return plot_script, plot_div
     return plot_text
 
+def plot_data_2():
+    """Plot the data in Bokeh"""
+
+    option = None
+    # get sensor data from the database
+    sensor_records=get_data('sensor_temps', option)
+
+    # get weather data
+    owm_records=get_data('owm_temps', option)
+
+    # get metoffice data
+    met_records=get_data('metoffice_temps', option)
+
+    p = figure(plot_width=800, plot_height=500, x_axis_type="datetime")
+
+    p.line([s[0] for s in sensor_records], [s[1] for s in sensor_records], color='blue',
+           legend='sensor')
+    p.line([o[0] for o in owm_records], [o[1] for o in owm_records], color='red',
+           legend='OWM')
+    p.line([m[0] for m in met_records], [m[1] for m in met_records], color='black',
+           legend='MetOffice')
+
+    # Jsonify the plot to put in html
+    plot_script, plot_div = components(p)
+
+    return plot_script, plot_div
+
 @app.route('/')
 def hello():
 
-    #plot_script, plot_div = plot_data()
+    plot_script, plot_div = plot_data_2()
     # CDN.render() has all of the information to get the javascript libraries
     # for Bokeh to work.
-    return render_template('test_template.html', resources=CDN.render())
+    return render_template('temp_graph.html', plot_div=plot_div,
+                           plot_script=plot_script, resources=CDN.render())
     #return page.render(resources=CDN.render())
