0ba259eded2ab1143306c035b72baa6c85e011f9
Emlyn Price <emlyn.je.price@gmail.com>
2019-01-03 19:54:58 +0000

    Change name of app in setup.py

--- src/web_interface/setup.py
+++ src/web_interface/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='monitor_temp',
+    name='tempmonitor',
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
