93021ccbc3ff32b558398520cfb9076d4fea496f
Emlyn Price <emlyn.je.price@gmail.com>
2019-01-09 18:59:01 +0000

    Set TimeForm choices to strings
    WTForms treats data it receives as strings if not told otherwise. Setting the
    choices to strings allows the form to be validated.

--- src/web_interface/webpage/TimeForm.py
+++ src/web_interface/webpage/TimeForm.py
@@ -5,11 +5,15 @@ class TimeForm(FlaskForm):
     """Class to manage the generation of the time selection form"""
 
     # Choices is a sequence of (value, label) pairs
+    # WTForms treats data as a string unless told otherwise, and then fails to
+    # validate if the values in choices are not strings. It is possible to tell
+    # WTForms that the data it gets are ints (or whatever), but for my purposes
+    # strings are more useful
     the_time = SelectField(u'Time range',
-                           choices = [(6, 'The last 6 hours'),
-                                      (12, 'The last 12 hours'),
-                                      (24, 'The last 24 hours'),
-                                      (48, 'The last 48 hours'),
-                                      (72, 'The last 72 hours'),
-                                      (168, 'The last 168 hours'),
+                           choices = [('6', 'The last 6 hours'),
+                                      ('12', 'The last 12 hours'),
+                                      ('24', 'The last 24 hours'),
+                                      ('48', 'The last 48 hours'),
+                                      ('72', 'The last 72 hours'),
+                                      ('168', 'The last 168 hours'),
                                       ('all', 'All records')])
