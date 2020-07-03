490e21605a8932227d58402133b0a6c77c94286c
Emlyn Price <emlyn.je.price@gmail.com>
2020-07-03 21:48:30 +0100

    Bump version

--- src/web_interface/setup.py
+++ src/web_interface/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='temp_monitor_web',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
